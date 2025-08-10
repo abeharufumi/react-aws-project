5d84a22fc3acf896f2b2f7008652859bdcb5094a
abeharufumi <abedah20@gmail.com>
2025-08-10 15:07:24 +0900

    [update]striptに接続

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext"; // ← AuthProviderをインポート
+import { AuthProvider } from "./context/AuthContext";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+// .envファイルから公開可能キーを読み込む
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </AuthProvider>
   </React.StrictMode>
 );
